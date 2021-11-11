2b6bb3e4876d097df850e59c86acb265ee62f24a
DavidFirth <davidf@veryspeedy.net>
2021-11-11 07:06:39 +0000

    Edit of the prior data function

--- R/make_prior_data.R
+++ R/make_prior_data.R
@@ -1,16 +1,25 @@
-make_prior_data <- function(league, season, prior_weight = 1){
+make_prior_data <- function(league, season, damping_weight, consistency_weight){
     teamNames <- read.csv(paste0(league, "/", season, "/", "namesOfTeams.csv"),
                           as.is = TRUE)
     nTeams <- nrow(teamNames)
     teamNames <- teamNames $ abbrev
     teamNames <- rep(teamNames, rep(2, nTeams))
     matchNames <- paste0(teamNames, rep(c("_home", "_away"), 2 * rep(nTeams, 2)))
-    result <- data.frame(match = paste0("prior.", matchNames),
+    result1 <- data.frame(match = paste0("prior.", matchNames),
                          matchday = 0,
                          homeTeam = c(teamNames, rep("average", length(teamNames))),
                          awayTeam = c(rep("average", length(teamNames)), teamNames),
                          FTR = rep(c("A", "H"), nTeams),
-                         count = prior_weight * rep(c(1, 1), nTeams),
+                         count = damping_weight * rep(c(1, 1), nTeams),
                          draw = 0, home = 0)
-    return(result)
+    matchNames <- teamNames
+cat("blah")
+    result2 <- data.frame(match = paste0("prior.", matchNames),
+                         matchday = 0,
+                         homeTeam = matchNames,
+                         awayTeam = matchNames,
+                         FTR = rep(c("A", "H"), nTeams),
+                         count = consistency_weight * rep(c(1, 1), nTeams),
+                         draw = 0, home = 0)
+    return(rbind(result1, result2))
 }
