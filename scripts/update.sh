53e0f85abeceec75ddb87a6f1b2c3c468503ef28
David <davidf@veryspeedy.net>
2023-03-24 10:59:31 +0000

    Slightly more robust handling of current season

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+## This script works in the up-to-date alt3 repository
+cd "$ALT3_HOME"
+
 league=$1
 export league
 
 notification="*$league*\n:large_blue_square: Starting an update run $(date +"%T")\n"
 
-cd "$ALT3_HOME"
-
 # git pull origin master
 # April 2021, try working without the automated pull, to avoid clashes
 
-Rscript "$ALT3_SCRIPTS"/update.R $league 2022
+Rscript "$ALT3_SCRIPTS"/update.R $league $ALT3_CURRENT_SEASON
 
 #add, commit and push files
 
@@ -47,6 +48,7 @@ curl -X POST -H 'Content-type: application/json' --data \{\"text\":\""$notificat
 echo "
 "
 
+## Still need to push the commits up to alt3 on github
 exit 0 
 
 
