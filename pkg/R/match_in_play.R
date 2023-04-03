11d66359cdd8f4692334f318d1a0b4bfc346bc07
David <davidf@veryspeedy.net>
2023-04-03 11:54:10 +0100

    Added match_in_play function

---
+++ pkg/R/match_in_play.R
@@ -0,0 +1,6 @@
+match_in_play <- function(league, season, resuults = "latest.csv") {
+    teams_and_results <- alt3_data(league, season, results)
+    results <- teams_and_results $ results
+    in_play <- results$status %in% c("IN_PLAY", "PAUSED")
+    return(any(in_play))
+}
